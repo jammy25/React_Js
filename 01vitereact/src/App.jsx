dad516facbd96cb4f1706d58472d7405fcaaeb8a
Kishan <kishan95.jammy@gmail.com>
2023-09-25 22:03:53 +0530

    exploring BTS of creating element with React, its syntax and rendering

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,17 @@ import MakingChai from "./chai"
 
 function App() {
 
+  // Let's see how we can insert js and var inside, as we are doing with html
+  // we write inside {} - our var
+  // inside {} - we get EVALUATED EXPRESSION - we don't write JS here, instead we write the final outcome
+  
+  
+  const username = 'chai aur code'
   return (
     <>
     <MakingChai/>
     <h2>We can only export one element in JSX, so to solve this and send more elements we wrap them all in div which is again one element only. This was so common that they change div to empty fragments for better usability.</h2>
+    <p>test para {username}</p>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+/*
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: 'Click me to visit google' 
+}
+*/
+
+// ReactElemet obj did not execute as MyApp since its parameters will not be matching the rendering which is done React, in custom app we have done the same thing but also have created our own render method with similar param
+
+// Let's see if we provide ReactElement params in same format as MyApp then will it work
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+// So we can say there is conversion going on behind the scene
+
+// Now let's create the reactElement which is similar to how React elements are created to understand what type of props they are using
+// To create that we will need React and here we have to follow their syntax and not use things like type, children etc.
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click to visit Google'
+)
+
+// So to conclude we can say that we could insert obj to run customreact file but we have to follow React syntax
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+    
+    // <MyApp/>
+    // anotherElement
+    // reactElement
+
+    <App/>
 
 )
