098f0b8629c364ca2dd2efa92c7286e31cc46664
Kishan <kishan95.jammy@gmail.com>
2023-09-29 22:20:40 +0530

    number and character for pw set

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("  ")
 
   const passwordGenerator = () => {}
 
