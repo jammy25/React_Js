4104e89bb9caecf4b9d4750b0ec31d9dc78da675
Kishan <kishan95.jammy@gmail.com>
2023-10-21 23:50:31 +0530

    updated

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -7,14 +7,15 @@ function App() {
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
+                        {/* themebtn */}
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       
+                       {/* Card */}
                     </div>
                 </div>
             </div>
+
   )
 }
 
