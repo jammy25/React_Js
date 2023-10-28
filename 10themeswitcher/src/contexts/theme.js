b421b6b70f53adfee14e5f50c1c32d95861aa1a1
Kishan <kishan95.jammy@gmail.com>
2023-10-28 22:41:06 +0530

    updated

--- 10themeswitcher/src/contexts/theme.js
+++ 10themeswitcher/src/contexts/theme.js
@@ -2,5 +2,6 @@ import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
     themeMode: "Light",
-    darkTheme: () => {}
+    darkTheme: () => {},
+    lightTheme: () => {}
 })
