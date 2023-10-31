40be44ed3e66385251d26d6b09bfedf1a107a1ee
Kishan <kishan95.jammy@gmail.com>
2023-10-31 23:12:25 +0530

    updated

--- 10themeswitcher/src/contexts/theme.js
+++ 10themeswitcher/src/contexts/theme.js
@@ -9,5 +9,5 @@ export const ThemeContext = createContext({
 export const ThemeProvider = ThemeContext.Provider
 
 export default function useTheme(){
-    return useContext
+    return useContext(ThemeContext)
 }
