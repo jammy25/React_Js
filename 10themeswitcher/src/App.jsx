02e5a669f05042e13547df2693d02fee7e9644ff
Kishan <kishan95.jammy@gmail.com>
2023-11-01 23:04:25 +0530

    updated

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -1,22 +1,20 @@
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
-
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* themeBtn */}
-                    </div>
+    <ThemeProvider>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       {/* Card */}
-                    </div>
-                </div>
-            </div>
-
-  )
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
