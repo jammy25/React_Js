7e552701facdbe4925b29a15f79cee447355a054
Kishan <kishan95.jammy@gmail.com>
2023-11-02 22:44:14 +0530

    updated

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "./contexts/theme";
 
 function App() {
   return (
-    <ThemeProvider>
+    <ThemeProvider value={{themeMode}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
