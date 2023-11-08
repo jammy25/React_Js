a1e64647fcd7682e4dfedc5b3196694afa4cf29f
Kishan <kishan95.jammy@gmail.com>
2023-11-08 22:12:19 +0530

    updated

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
 
 useEffect(() => {
   document.querySelector('html').classList.remove("light", "dark")
+  document.querySelector('html').classList.add("themeMode")
 })
 
   return (
